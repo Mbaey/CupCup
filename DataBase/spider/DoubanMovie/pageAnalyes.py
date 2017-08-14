8ae1ec590c6a1b4129f0678234660744698fa5b7
Mbaey <1092460929@qq.com>
2017-08-14 12:08:55 +0800

    bs4新的用法
    初学bs4还不会用。
    不过能爬下来就好呢、

--- DataBase/spider/DoubanMovie/pageAnalyes.py
+++ DataBase/spider/DoubanMovie/pageAnalyes.py
@@ -21,6 +21,23 @@ def pageAna(tag, URL, directors, rate, title, casts, cover):
         summary = soup.find_all(attrs={"property": "v:summary"})
         summary = summary[0].get_text()
 
+        #当时学的时候还不知道
+        #info = soup.select('#info')
+        #直接返回的就是一个beautifulsoup的对象啊。。。
+        #info = html.select('#info')[0]
+		#info = info.get_text().split('\n')
+
+		# 提取字段，只要冒号后面的文本内容
+		#director = info[1].split(':')[-1].strip()
+		#composer = info[2].split(':')[-1].strip()
+		#actor = info[3].split(':')[-1].strip()
+		#category = info[4].split(':')[-1].strip()
+		#district = info[6].split(':')[-1].strip()
+		#language = info[7].split(':')[-1].strip()
+		#showtime = info[8].split(':')[-1].strip()
+		#length = info[9].split(':')[-1].strip()
+		#othername = info[10].split(':')[-1].strip()
+        
         info = soup.find_all(id='info')
         soup = BeautifulSoup(str(info[0]), "html.parser")
         adaptor = soup.find_all(attrs={"class": "attrs"})
